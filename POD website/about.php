2a3c2f064776a3404ac687f73f4f10ad9b7e2aaa
kiranv04 <128119379+kiranv04@users.noreply.github.com>
2023-08-24 21:54:54 +0530

    Add files via upload

---
+++ POD website/about.php
@@ -0,0 +1,9 @@
+<?php 
+$activepage = 'about';
+include_once 'header.php'; ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12"></div>
+    </div>
+</div>
+<?php include_once 'footer.php'; ?>
---
+++ POD website/functions/ui-functions.js
@@ -0,0 +1,16 @@
+// Function to show navbar in mobile screen
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+// Function to close navbar in mobile screen
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+// Function to open search in mobile screen
+function openSearch() {
+    document.getElementById("searchOverlay").style.display = "block";
+}
+// Function to close search in mobile screen
+function closeSearch() {
+    document.getElementById("searchOverlay").style.display = "none";
+}
